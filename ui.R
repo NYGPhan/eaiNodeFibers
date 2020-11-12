4692cbc92ff635fcb8339ff0b4cd6130616877ac
Adam Mandelson <adam.mandelson@gmail.com>
2020-11-12 15:29:33 -0500

    added url

--- ui.R
+++ ui.R
@@ -3,9 +3,11 @@ navbarPage(
   theme = "bootstrap.css",
   
   title = 'US FiNE Node Fibers',
-  tabPanel('Current Fibers',     DT::dataTableOutput('fibers'))
-  # tabPanel('Length menu',        DT::dataTableOutput('ex2')),
-  # tabPanel('No pagination',      DT::dataTableOutput('ex3')),
-  # tabPanel('No filtering',       DT::dataTableOutput('ex4')),
-  # tabPanel('Function callback',  DT::dataTableOutput('ex5'))
+  
+  tabPanel('Current Fibers',
+           tags$p("US Network Fibers"),
+           tags$a(href="https://github.com/NYGPhan/eaiNodeFibers", "Source"),
+           tags$p(),
+           DT::dataTableOutput('fibers'))
+
 )
